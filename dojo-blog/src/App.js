90e750707cd01048fe52d07dc0d668a4d0108ff8
elnura15ae <elnura15ae@gmail.com>
2021-01-26 22:01:27 +0100

    updated

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -13,18 +13,6 @@ const link ="http://www.google.com"
       <Navbar />
       <div className="content"></div>
       <Home />
-      <h1>App Component</h1>
-      <h1>{ title }</h1>
-      <p>Liked { likes }</p>
-
-      {/* <p> { person } */}
-
-      <p>{ 10 }</p>
-      <p>{ "hello, Ninjas" }</p>
-      <p>{ [1,2,3,4,5] }</p>
-      <p>{ Math.random() * 10 }</p>
-
-      <a href={link}>Google Site</a>
     </div>
   );
 }
