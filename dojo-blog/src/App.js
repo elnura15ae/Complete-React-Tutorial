6a1bff571f8b1e833e8edff245a87486b16716f5
elnura15ae <elnura15ae@gmail.com>
2021-01-26 20:28:50 +0100

    updated

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const title= 'Welcome to the new blog!'
+const likes = 50;
+// const person = { name:'yoshi', age:30} error
+const link ="http://www.google.com"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content"></div>
+      <h1>App Component</h1>
+      <h1>{ title }</h1>
+      <p>Liked { likes }</p>
+
+      {/* <p> { person } */}
+
+      <p>{ 10 }</p>
+      <p>{ "hello, Ninjas" }</p>
+      <p>{ [1,2,3,4,5] }</p>
+      <p>{ Math.random() * 10 }</p>
+
+      <a href={link}>Google Site</a>
     </div>
   );
 }
