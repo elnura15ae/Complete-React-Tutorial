64c6223d52ad557dd5641f1f2d9b2ba7bcc05113
elnura15ae <elnura15ae@gmail.com>
2021-01-26 21:41:07 +0100

    updated

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,21 @@
-const Home = () => {
-    const handleClick=(e)=>{
-        console.log('hello, ninjas', e)
-    }
+import {useState} from 'react';
 
-    const handleClickAgain = (name, e) =>{
-        console.log('hello' + name, e.target)
+const Home = () => {
+    
+    const [name, setName]= useState('mario')
+    const [age, setAge] = useState(25)
+
+    const handleClick=()=>{
+      
+        setName('lzuigi');
+        setAge(30)
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is {age} years old.</p>
             <button onClick={handleClick}>Click me </button>
-            <button onClick={(e)=> handleClickAgain(' mario', e)}>Click me again</button>
         </div>
      );
 }
